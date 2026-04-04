afe616dcbaed50e0aa1a087d927feae33b66595d
yashijoe <yashijoe@users.noreply.github.com>
2026-04-04 14:50:28 +0200

    Change entity name attribute to False

--- custom_components/rainmachine_pro/sensor.py
+++ custom_components/rainmachine_pro/sensor.py
@@ -76,7 +76,7 @@ async def async_setup_entry(
 class RainMachineBaseEntity(CoordinatorEntity):
     """Base entity for RainMachine Pro."""
 
-    _attr_has_entity_name = True
+    _attr_has_entity_name = False
 
     def __init__(
         self,
